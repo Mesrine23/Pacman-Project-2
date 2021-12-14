9091348ef65512b3a83d5b3375800b42e21513a8
Panagiotis Palamidas <75530751+Mesrine23@users.noreply.github.com>
2021-12-14 13:26:59 +0200

    Delete tests directory

--- tests/kappa.py
+++
@@ -1,2 +0,0 @@
-minFoodist = [2,23,1,6,3,77,2,0,1,0,1,654]
-print(min(minFoodist))
--- tests/temp.py
+++
@@ -1,6 +0,0 @@
-dist = []
-for corner in corners:
-    if corner not in state[1]:
-        dist.append(util.manhattanDistance(state[0],corner))
-
-return max(dist)Σ
